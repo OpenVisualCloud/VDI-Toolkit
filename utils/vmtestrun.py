a90ba351e60543da28616080f60586777ca8cd97
qiaoliu78 <qiao.liu@intel.com>
2022-12-07 14:27:11 +0800

    Add remote connection support (#11)

--- utils/vmtestrun.py
+++ utils/vmtestrun.py
@@ -3,30 +3,82 @@
 
 import os
 import sys
+import getopt
 import subprocess
 import time
 iplist=[]      
-processlist=[]      
+processlist=[]
+ipfile="vm_ip.txt"
+rdp_processlist=[]      
 def find_vm_ip():
     global iplist
-    for i in open("vm_ip.txt"):
+    global ipfile
+    for i in open(ipfile):
         ip=i.rstrip('\n')
         iplist.append(ip)
+
+def help():
+    print """
+    Usage: vmtestrun [options]"
+    \t-u: user name
+    \t-p: password
+    \t-c: connection type RDP|VNC [default : RDP]
+    \t-f: iplist file  [default : vm_ip.txt]
+    """
             
 def main():
     global iplist
+    global ipfile
+    connect_type = "RDP"
+    username = ""
+    password = ""
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hp:c:u:f:")
+    except getopt.GetoptError:
+        help()
+    for opt, arg in opts:
+        if opt == "-h":
+            help()
+            sys.exit()
+        elif opt == "-p":
+            password = opt+arg
+        elif opt == "-c":
+            connect_type = arg
+        elif opt == "-u":
+            username = opt+arg
+        elif opt == "-f":
+            ipfile = arg
+            
     find_vm_ip()
     systype=sys.platform
     print(systype)
     print(iplist)
+    
+    if connect_type == "RDP":
+        connect_py = "/usr/bin/rdpy-rdpclient.py"
+    else:
+        connect_py = "/usr/bin/rdpy-vncclient.py"
+        username=""
+    if username =="" and password =="":
+        connect_py =""
     for vm_ip in iplist:
-        print vm_ip
-        if systype == "linux2":
+        print(vm_ip)
+        print(systype)
+        if systype == "linux" or systype == "linux2":
             p=subprocess.Popen(["python2.7", "winapptest.py",vm_ip])
         else:
             p=subprocess.Popen(["python", "winapptest.py",vm_ip],shell=True)
-        processlist.append(p); 
+        processlist.append(p);
         time.sleep(0.05)
+        if len(connect_py) != 0:
+            if systype == "linux" or systype == "linux2":
+                p=subprocess.Popen(["python2.7", connect_py,username,password,vm_ip])
+            else:
+                p=subprocess.Popen(["python", connect_py,username,password,vm_ip],shell=True)
+            rdp_processlist.append(p);
+            time.sleep(0.05)
+
+
     while True:
         i=0
         for vm_ip in iplist:
@@ -34,18 +86,34 @@ def main():
             str="vmip {} process status {}"
             status=p.poll()
             if status == None:
-            	  print(str.format(vm_ip," OK"))
+                  print(str.format(vm_ip," OK"))
             else:
-            	  print(str.format(vm_ip,p.returncode))
-            	  processlist.pop(i)
-                  if systype == "linux2":
+                  print(str.format(vm_ip,p.returncode))
+                  processlist.pop(i)
+                  if systype == "linux" or systype == "linux2":
                       p=subprocess.Popen(["python2.7", "winapptest.py",vm_ip])
                   else:
                       p=subprocess.Popen(["python", "winapptest.py",vm_ip],shell=True)
-                  processlist.insert(i,p); 
-            i=i+1
+                  processlist.insert(i,p);
             time.sleep(2)
-    if systype == "linux2":
+            if len(connect_py) != 0:
+                p=rdp_processlist[i]
+                str="vmip {} {} process status {}"
+                status=p.poll()
+                if status == None:
+                    print(str.format(vm_ip,connect_type," OK"))
+                else:
+                    print(str.format(vm_ip,connect_type,p.returncode))
+                    rdp_processlist.pop(i)
+                    if systype == "linux" or systype == "linux2":
+                        p=subprocess.Popen(["python2.7",connect_py,username,password,vm_ip])
+                    else:
+                        p=subprocess.Popen(["python", connect_py,username,passowrd,vm_ip],shell=True)
+                    rdp_processlist.insert(i,p);
+                time.sleep(2)
+            i=i+1
+
+    if systype == "linux" or systype == "linux2":
         testinput=input()
     else:
         os.system('pause')
