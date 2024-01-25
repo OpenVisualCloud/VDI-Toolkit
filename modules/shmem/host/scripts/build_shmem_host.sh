1be0ca9f619947c0deb4214f1e96727a591f8c2a
Luo Ying <ying2.luo@intel.com>
2024-01-25 10:09:24 +0800

    Add shmem host build script to build oneVPL and console components. (#58)
    Signed-off-by: luoying1234 <ying2.luo@intel.com>

---
+++ modules/shmem/host/scripts/build_shmem_host.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -ex
+
+SRC_PATH="$PWD"/..
+
+build_oneVPL()
+{
+    cd "$SRC_PATH"
+    oneVPLDir="oneVPL"
+    if [[ ! -d "$oneVPLDir" ]];then
+        git clone https://github.com/intel/libvpl.git "$oneVPLDir"
+    fi
+    cd "$oneVPLDir"
+    git checkout v2023.3.1
+    git am --ignore-whitespace ../oneVPL-patches/0001-Add-share-memory-file-process-in-sample-encode.patch
+    sudo script/bootstrap
+    source /opt/rh/devtoolset-7/enable
+    script/build
+}
+
+build_console()
+{
+    buildDir="build"
+    cd "$SRC_PATH"/console
+    if [[ ! -d "$buildDir" ]];then
+        mkdir "$buildDir"
+    fi
+    cd "$buildDir"
+    cmake .. && make
+}
+
+build_host()
+{
+    build_oneVPL
+    build_console
+}
+
+build_host
